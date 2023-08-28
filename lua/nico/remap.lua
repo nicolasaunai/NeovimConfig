0c604190adbc72147bfff3a2639155311835eb8f
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2023-08-28 17:44:43 +0200

    fix keymap
     Committer: Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>

--- lua/nico/remap.lua
+++ lua/nico/remap.lua
@@ -6,7 +6,7 @@ local set = vim.keymap.set
 
 
 set("n", "<leader>pf", vim.cmd.Ex)
-set("n", "n", "<leader>sq", 'ciw""<Esc>P', { desc = "Word Surround Quotes" })
+set("n", "<leader>sq", 'ciw""<Esc>P', { desc = "Word Surround Quotes" })
 
 
 -- splits ----------------------------------------------------------------------
