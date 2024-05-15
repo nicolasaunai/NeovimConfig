789c83d822ef48b42ed768cbc2c70d41784f91d9
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2024-05-15 09:58:34 +0200

    highlight on yank

--- lua/nico/remap.lua
+++ lua/nico/remap.lua
@@ -212,3 +212,14 @@ vim.keymap.set("i", "<leader><Tab>", "copilot#Accept()", {expr=true, silent=true
 
 
 
+
+vim.api.nvim_create_autocmd("TextYankPost", 
+    {
+        desc= "Highlight on yank",
+        group = vim.api.nvim_create_augroup( "HighlightOnYank", {clear=true}),
+        callback= function ()
+            vim.highlight.on_yank()
+        end
+    })
+
+
